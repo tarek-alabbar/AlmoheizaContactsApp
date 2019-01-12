a6d545bb6ebacc1c6acc4cf0815055fe5b927860
Mohamad Tarek Alabbar <Alabbar.tarek@gmail.com>
2019-01-12 10:19:47 +0000

    Add project files.

---
+++ AlmoheizaContactsApp/AlmoheizaContactsApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AlmoheizaContactsApp.Models;
+
+namespace AlmoheizaContactsApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
