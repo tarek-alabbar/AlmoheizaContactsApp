2be9b1ec5af27be9a9014df633f77d0e6fb0d5c2
Mohamad Tarek Alabbar <Alabbar.tarek@gmail.com>
2019-01-12 13:48:48 +0000

    Added URL class

---
+++ AlmoheizaContactsApp/AlmoheizaContactsApp/Services/Connection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlmoheizaContactsApp.Services
+{
+    public static class Connection
+    {
+
+        public static string AppURL = @"https://almoheizaphonebookapp.azurewebsites.net";
+    }
+}
