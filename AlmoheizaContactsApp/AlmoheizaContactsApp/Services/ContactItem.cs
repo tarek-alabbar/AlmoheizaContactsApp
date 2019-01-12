8b273d259edc50a7c213f0c94f7c2426b73269e4
Mohamad Tarek Alabbar <Alabbar.tarek@gmail.com>
2019-01-12 13:56:37 +0000

    Azure Client Installed

---
+++ AlmoheizaContactsApp/AlmoheizaContactsApp/Services/ContactItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlmoheizaContactsApp.Services
+{
+    public class ContactItem
+    {
+        string id;
+        string name;
+        string city;
+        string job;
+        string number;
+        string email;
+        string sons;
+
+
+    }
+}
